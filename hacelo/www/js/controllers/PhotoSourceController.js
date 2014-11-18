aa4a0ae324720948e5e4af907b95c59627f6591f
josephma93 <josephma93@gmail.com>
2014-11-18 00:41:20 -0600

    Updating choose screen
    I'm adding my latest changes wish includes:
    Improved Instagram service.
    New Cordova camera Service.
    As well updating choose screen controller and their HTML partial.
    And others.

--- hacelo/www/js/controllers/PhotoSourceController.js
+++ hacelo/www/js/controllers/PhotoSourceController.js
@@ -1,16 +1,83 @@
-controllers.controller('PhotoSourceCtrl', ['$scope', '$ionicPopup', 'MessageService', 'InstagramService', 'CordovaCameraService', 'ImageFactory', function ($scope, $ionicPopup, MessageService, InstagramService, CordovaCameraService, ImageFactory) {
-    $scope.pickedImages = [];
+controllers.controller('PhotoSourceCtrl', ['$scope', '$filter', '$ionicPopup', '$ionicLoading', 'SelectedImagesFactory', 'MessageService', 'InstagramService', 'CordovaCameraService', 'ImageFactory', function ($scope, $filter, $ionicPopup, $ionicLoading, SelectedImagesFactory, MessageService, InstagramService, CordovaCameraService, ImageFactory) {
+    var lastInstagramLoad;
 
-    $scope.instagramPick = function() {
-        InstagramService.auth();
+    $scope.loading = false;
+    $scope.imageStack = [];
+    $scope.$watch('loading', function(newVal, oldVal) {// for showing and hiding load spinner
+        var cache = angular.isDefined(cache)? cache: MessageService.search("loading");
+        if (newVal !== oldVal) {
+            if (newVal === true) {
+                $ionicLoading.show(cache);
+            } else {
+                $ionicLoading.hide();
+            }
+        }
+    });
+    $scope.$watch('imageStack', function(newVal, oldVal) {// to keep updated the Service
+        SelectedImagesFactory.setSelectedImages($scope.imageStack);
+    }, true);
+
+    function extractInstagramImages (apiResponse) {
+        lastInstagramLoad = apiResponse;
+        var filteredResponse = $filter('filter')(apiResponse.data, {type:"image"}),
+            j = filteredResponse.length;
+
+        if (j === 0) {
+            $ionicPopup.alert(MessageService.search("no-images-found"));
+        } else {
+            for (var i = 0; i < j; i++) {
+                $scope.imageStack.push(new ImageFactory(filteredResponse[i].images));
+            }
+        }
+    }
+
+    function getRecentMedia (){
+        $scope.loading = true;
+        InstagramService.getRecentMedia()
+            .then(function(response) {
+                $scope.loading = false;
+                if (response.data.meta.code === 400) { // check if token is expired
+                    authenticateInstagramUser();
+                } else {
+                    extractInstagramImages(response.data);
+                }
+            }, function(err) {
+                $scope.loading = false;
+                $ionicPopup.alert(MessageService.search("cannot-load-media"));
+            });
+    }
+
+    function authenticateInstagramUser (){
+        InstagramService.auth()
+            .then(function(result) {
+                getRecentMedia();
+            }, function(err) {
+                $ionicPopup.alert(MessageService.search("user-denied-access"));
+            });
+    }
+
+    $scope.instagramImageLoad = function() {
+        if (InstagramService.isAuthenticated()){
+            getRecentMedia();
+        } else {
+            authenticateInstagramUser();
+        }
     };
-    $scope.phonePick = function() {
-        CordovaCameraService.getPhonePic()
-        .then(function(result) {
-            alert(result);
-            $scope.pickedImages.push(result);
-        }, function(result) {
-            $ionicPopup.alert(MessageService.search("cordova-load-failed"));
-        });
+
+    $scope.phoneImageLoad = function() {
+        CordovaCameraService.getImage()
+            .then(function(result) {
+                $scope.imageStack.push(new ImageFactory(result));
+            }, function(result) {
+                // $ionicPopup.alert(MessageService.search("cordova-load-failed"));
+            });
     };
+
+    $scope.logArray = function(){
+        console.log($scope.imageStack);
+    };
+
+    InstagramService.getRecientTagMedia().then(function(response){
+        extractInstagramImages(response.data);
+    },function(err){});
 }]);
--- hacelo/www/js/services/InstagramService.js
+++ hacelo/www/js/services/InstagramService.js
@@ -1,87 +1,113 @@
-services.service('InstagramService', ['$http','$window','$ionicPopup','MessageService', function ($http, $window, $ionicPopup, MessageService) {
+services.service('InstagramService', ['$http', '$window', '$q', function ($http, $window, $q) {
     var self = this,
-        info,
         user,
         accessToken,
         config = {
-            clientId:'70a2ae9262fc4805a5571e8036695a4d',
-            redirectUri:'http://www.wikipedia.org/',
+            clientId: '70a2ae9262fc4805a5571e8036695a4d',
+            redirectUri: 'http://www.wikipedia.org/',
             apiUrl: 'https://api.instagram.com/v1/',
             oauthUrl: 'https://instagram.com/oauth/authorize/?',
             scope: 'basic'
         };
 
-    var fetch = function(url, callback, params, method) {
+    this.isAuthenticated = function () {
+        return angular.isDefined(accessToken);
+    };
+
+    var fetch = function (url, params) {
         var prms = {
                 client_id: config.clientId,
                 callback: 'JSON_CALLBACK'
-            },
-            cnfg = {
-                url: config.apiUrl+url,
-                method: method||'jsonp',
+            };
+        if (self.isAuthenticated()) {
+            prms.access_token = accessToken;
+        }
+        var cnfg = {
+                url: config.apiUrl + url,
+                method: 'jsonp',
                 responseType: 'json',
                 params: angular.extend(prms, params)
             };
 
-        $http(cnfg)
-            .success(function(data){
-                callback(data);
-            }).error(function(data){
-                callback({});
-            });
+        return $http(cnfg);
+    };
+
+    var getUrlParameters = function (parameter, staticURL, decode) {
+        /*
+        Function: getUrlParameters
+        Description: Get the value of URL parameters either from 
+                     current URL or static URL
+        Author: Tirumal
+        URL: www.code-tricks.com
+       */
+       var currLocation = (staticURL.length)? staticURL : window.location.search,
+           parArr = currLocation.split('?')[1].split('&'),
+           returnBool = true,
+           parr;
+       
+       for (var i = 0; i < parArr.length; i++){
+            parr = parArr[i].split('=');
+            if(parr[0] == parameter){
+                return (decode) ? decodeURIComponent(parr[1]) : parr[1];
+                returnBool = true;
+            }else{
+                returnBool = false;            
+            }
+       }
+       
+       if(!returnBool) return false;
     };
 
     var bindAuthEvents = function(authTab) {
         // This function bind the evens to the instagram autentication screen
         // If every thing goes good then load the user information
         // Also handle how the app respond to an authentication error
-        alert(authTab);
-        authTab.addEventListener('loadstop', function(e) {
+        var deferred = $q.defer();
+        authTab.addEventListener('loadstart', function(e) {
             if (e.url.search('access_token') !== -1) { // access granted
-                accessToken = e.url.substr(e.url.search('access_token')+13);
+                accessToken = e.url.substr(e.url.search('access_token')+'access_token='.length);
+                authTab.close();
+                deferred.resolve({
+                    authorized: true
+                });
+            } else if (e.url.search('error') !== -1) { // The user denied access
+                authTab.close();
+                deferred.reject({
+                    error: getUrlParameters('error',e.url,true),
+                    errorReason: getUrlParameters('error_reason',e.url,true),
+                    errorDescription: getUrlParameters('error_description',e.url,true)
+                });
             }
-            if (e.url.search('error') !== -1) { // The user denied access
-                var popupConfig = MessageService.search("user-denied-access");
-                $ionicPopup.show(popupConfig);
-            }
-            authTab.close();
         });
-    };
-
-    this.getToken=function(){
-        return $window.sessionStorage.getItem('tkn');
+        return deferred.promise;
     };
 
     this.auth = function() {
         var options = {
-            client_id : config.clientId,
-            redirect_uri : config.redirectUri,
-            scope : config.scope,
-            response_type: 'token'
-        };
-
-        var authParams='';
+                client_id:      config.clientId,
+                redirect_uri:   config.redirectUri,
+                scope:          config.scope,
+                response_type:  'token'
+            },
+            authParams = '';
 
         angular.forEach(options, function(value, key) {
             return authParams += key + '=' + value + '&';
         });
+        var authUrl = config.oauthUrl + authParams;
 
-        var authUri = config.oauthUrl + authParams;
-
-        bindAuthEvents($window.open(authUri, '_blank', 'location=yes'));
+        return bindAuthEvents($window.open(authUrl, '_blank', 'location=yes'));
     };
 
-    this.getMedia = function(callback, params) {
-        fetch('users/self/media/recent', callback, angular.extend({
-            access_token: accessToken
-        }, params));
+    this.getRecentMedia = function(params) {
+        return fetch('users/self/media/recent', params);
     };
 
-    this.currentUser = function(callback) {
-        fetch('users/self', function(data) {
-            user = data.data;
-        }, {
-            access_token: accessToken
-        });
+    this.getCurrentUser = function() {
+        return fetch('users/self');
+    };
+
+    this.getRecientTagMedia = function() {
+        return fetch('tags/angularjs/media/recent');
     };
 }]);
