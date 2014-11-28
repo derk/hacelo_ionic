e4f7107ac63f9ca0ab56702ed66bbd036e1d5837
josephma93 <josephma93@gmail.com>
2014-11-27 21:42:59 -0600

    Solving Instagram photo selection bug
    I have solved the issue that Raiam found in the instagram photo
    selection screen.
    Also I'm deleting some unused files.

--- hacelo/www/js/app.js
+++ hacelo/www/js/app.js
@@ -4,7 +4,6 @@ angular.module('hacelo', [
     'hacelo.config',
     'hacelo.models',
     'hacelo.controllers',
-    'hacelo.providers',
     'hacelo.services'
 ])
 
--- hacelo/www/js/providers/module.js
+++
@@ -1,1 +0,0 @@
-var providers = angular.module('hacelo.providers', []);
